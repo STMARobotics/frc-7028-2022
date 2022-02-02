472a4e353011891406d2c9b229abbd6f97ad6939
STMA Robotics <7028robotics@mystma.org>
2022-02-02 16:46:56 -0600

    Drivetrain Session 1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class DrivetrainConstants {
+        public static final int LEFT_LEADER_ID = 0;
+        public static final int LEFT_FOLLOWER_ID = 1;
+        public static final int RIGHT_LEADER_ID = 2;
+        public static final int RIGHT_FOLLOWER_ID = 3;
+
+    }
+}

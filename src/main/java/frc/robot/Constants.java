99a3e8e334580b2e53aa56255e8eefdd3f3da270
STMA Robotics <7028robotics@mystma.org>
2022-02-08 17:35:34 -0600

    Add joystick drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,9 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
@@ -14,24 +17,21 @@ import edu.wpi.first.math.util.Units;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
  * <p>
  * It is advised to statically import this class (or one of its inner classes)
- * wherever the
- * constants are needed, to reduce verbosity.
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
     public static final class DriveTrainConstants {
         public static final int DEVICE_ID_RIGHT_MASTER = 0;
         public static final int DEVICE_ID_RIGHT_SLAVE_ONE = 1;
-        public static final int DEVICE_ID_RIGHT_SLAVE_TWO = 2;
-        public static final int DEVICE_ID_LEFT_MASTER = 3;
-        public static final int DEVICE_ID_LEFT_SLAVE_ONE = 4;
-        public static final int DEVICE_ID_LEFT_SLAVE_TWO = 5;
+        public static final int DEVICE_ID_LEFT_MASTER = 2;
+        public static final int DEVICE_ID_LEFT_SLAVE_ONE = 3;
 
         public static final int EDGES_PER_ROTATION = 8192;
         public static final double WHEEL_DIAMETER_INCHES = 6d;
@@ -58,6 +58,41 @@ public final class Constants {
         public static final double OPEN_LOOP_RAMP = .25;
     }
 
+    public static final class ControllerConstants {
+        public static final int PORT_ID_DRIVER_CONTROLLER = 0;
+        public static final int PORT_ID_OPERATOR_CONSOLE = 1;
+    }
+
+    public static final class TrajectoryConstants {
+
+        // Max speed in meters per second
+        public static final double MAX_SPEED_AUTO = 3;
+
+        // Max acceleration in meters per second per second
+        public static final double MAX_ACCELERATION_AUTO = 2;
+
+        // Max voltage
+        public static final double MAX_VOLTAGE_AUTO = 11;
+
+        public static final DifferentialDriveVoltageConstraint VOLTAGE_CONSTRAINT = new DifferentialDriveVoltageConstraint(
+                FEED_FORWARD, DRIVE_KINEMATICS, MAX_VOLTAGE_AUTO);
+
+        // Baseline values for a RAMSETE follower in units of meters and seconds
+        public static final double RAMSETE_B = 2;
+        public static final double RAMSETE_ZETA = 0.7;
+    }
+
+    public static final class DriverConstants {
+
+        public static final double ROTATION_MULTIPLIER = 1;
+
+        public static final double SLOW_MODE_SPEED_MULTIPLIER = .6;
+        public static final double SLOW_MODE_ROTATION_MULTIPLIER = .9;
+
+        public static final double DEADBAND_HIGH = 0.12;
+        public static final double DEADBAND_LOW = -DEADBAND_HIGH;
+    }
+
     public static final class ArcadeConstants {
         // Max speed to drive in teleop in meters per second
         public static final double MAX_SPEED_ARCADE = 3.7;
@@ -72,22 +107,115 @@ public final class Constants {
         public static final double ROTATE_RATE_LIMIT_ARCADE = 3.0;
     }
 
-    public static final class TrajectoryConstants {
+    public static final class AimConstants {
 
-        // Max speed in meters per second
-        public static final double MAX_SPEED_AUTO = 3;
-    
-        // Max acceleration in meters per second per second
-        public static final double MAX_ACCELERATION_AUTO = 2;
-    
-        // Max voltage
-        public static final double MAX_VOLTAGE_AUTO = 11;
-    
-        public static final DifferentialDriveVoltageConstraint VOLTAGE_CONSTRAINT = 
-            new DifferentialDriveVoltageConstraint(FEED_FORWARD, DRIVE_KINEMATICS, MAX_VOLTAGE_AUTO);
-    
-        // Baseline values for a RAMSETE follower in units of meters and seconds
-        public static final double RAMSETE_B = 2;
-        public static final double RAMSETE_ZETA = 0.7;
-      }
+        public static final double kP = 0.5;
+        public static final double kD = 0.2;
+        public static final double AIM_TOLERANCE = 0.3;
+
+        public static final double RANGE_HIGH = 1;
+        public static final double RANGE_LOW = -1;
+    }
+
+    public static final class LimeLightConstants {
+
+        public static final double TARGET_X_MAX = 29.8;
+
+        public static final double TARGET_ACQUIRED = 1.0;
+
+        public static final double PIPELINE_INDEX_NEAR = 0.0;
+        public static final double PIPELINE_INDEX_FAR = 1.0;
+
+        /** Height of the target in meters */
+        public static final double TARGET_HEIGHT = Units.inchesToMeters(80.875);
+
+        /** Height of the limelight on the bot in meters */
+        public static final double HIGH_MOUNT_HEIGHT = Units.inchesToMeters(23);
+
+        /** Distance Limelight is mounted from the front frame of the bot */
+        public static final double HIGH_DISTANCE_FROM_FRONT = Units.inchesToMeters(18);
+
+        /** Distance Limelight is mounted from the centerline of the bot */
+        public static final double HIGH_DISTANCE_FROM_CENTER = Units.inchesToMeters(0);
+
+        /** Angle of the limelight in degrees */
+        public static final double HIGH_MOUNT_ANGLE = 21.25;
+
+        public static final String HIGH_NAME = "limelight-high";
+
+        public static final String LOW_NAME = "limelight-low";
+    }
+
+    public static final class ControlPanelConstants {
+
+        public static final int DEVICE_ID_CONTROL_PANEL = 10;
+        public static final int DEVICE_ID_CONTROL_PANEL_ARM = 11;
+
+        public static final int EDGES_PER_ROTATION = 8192;
+
+        public static final double kS = 2.09;
+        public static final double kV = 0.109;
+        public static final double kA = 0.172;
+
+        public static final SimpleMotorFeedforward FEED_FORWARD = new SimpleMotorFeedforward(kS, kV, kA);
+
+        public static final double kP_VELOCITY = 0.1;
+        public static final double kP_POSITION = 1;
+        public static final double kD_POSITION = 0;
+
+        public static final double ROTATE_RPM = 200.0;
+        public static final double SET_COLOR_RPM = 180.0;
+
+        public static final int ARM_UP_POSITION = 0;
+        public static final int ARM_DOWN_POSITION = -1900;
+    }
+
+    public static final class ShooterConstants {
+
+        public static final int DEVICE_ID_SHOOTER_MASTER = 2;
+        public static final int DEVICE_ID_SHOOTER_SLAVE = 1;
+
+        public static final int CLOSED_LOOP_ERROR_RANGE = 20;
+
+        public static final double kS = 0.184;
+        public static final double kV = 0.125;
+        public static final double kA = 0.05;
+
+        public static final double kP = 0.0006;
+        public static final double RAMP_RATE = 0.2;
+
+        public static final double SHOOT_TIME = 0.25;
+
+    }
+
+    public static final class IndexerConstants {
+
+        public static final int DEVICE_ID_BELT = 6;
+
+        public static final int PORT_ID_INTAKE_SENSOR = 0;
+        public static final int PORT_ID_SPACER_SENSOR = 1;
+        public static final int PORT_ID_FULL_SENSOR = 2;
+
+        public static final double BELT_kP = .01;
+        public static final double BELT_kF = 1023;
+        public static final double BELT_RUN_SPEED = 11000;
+
+    }
+
+    public static final class IntakeConstants {
+
+        public static final int DEVICE_ID_INTAKE = 7;
+
+    }
+
+    public static final class TestModeConstants {
+        public static final String NetworkTableName = "Test Mode";
+    }
+
+    public static final class ClimbConstants {
+
+        public static final int DEVICE_ID_CLIMB = 3;
+
+    }
+
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,51 +6,46 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.commands.TeleDriveCommand;
+import frc.robot.subsystems.DriveTrainSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ShooterSubsystem m_exampleSubsystem = new ShooterSubsystem();
+  private final DriveTrainSubsystem driveTrainSubsystem = new DriveTrainSubsystem();
 
-  private final XboxController mController= new XboxController(0);
+  private final XboxController driverController = new XboxController(0);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final TeleDriveCommand teleDriveCommand = new TeleDriveCommand(driverController, driveTrainSubsystem);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+    configureSubsystemCommands();
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(mController, XboxController.Button.kA.value)
-    .whenHeld(new RunCommand(()->m_exampleSubsystem.runShooter(15000), m_exampleSubsystem))
-    .whenReleased(new RunCommand(()->m_exampleSubsystem.stop(), m_exampleSubsystem));
 
-    new JoystickButton(mController, XboxController.Button.kB.value)
-    .whenHeld(new RunCommand(()->m_exampleSubsystem.runShooter(15500), m_exampleSubsystem))
-    .whenReleased(new RunCommand(()->m_exampleSubsystem.stop(), m_exampleSubsystem));
-
-    new JoystickButton(mController, XboxController.Button.kX.value)
-    .whenHeld(new RunCommand(()->m_exampleSubsystem.runShooter(16000), m_exampleSubsystem))
-    .whenReleased(new RunCommand(()->m_exampleSubsystem.stop(), m_exampleSubsystem));
-
-    new JoystickButton(mController, XboxController.Button.kY.value)
-    .whenHeld(new RunCommand(()->m_exampleSubsystem.runShooter(20000), m_exampleSubsystem))
-    .whenReleased(new RunCommand(()->m_exampleSubsystem.stop(), m_exampleSubsystem));
   }
 
   /**
@@ -61,5 +56,9 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return null;
+  }
+
+  private void configureSubsystemCommands() {
+    driveTrainSubsystem.setDefaultCommand(teleDriveCommand);
   }
 }

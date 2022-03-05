37674bc17aad48de030331bccd8bc2822b6cb231
Andrew Gasser <agasser01@hotmail.com>
2022-03-05 16:35:52 -0600

    Configure through bore encoder

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -5,20 +5,26 @@ import static frc.robot.Constants.IndexerConstants.DEVICE_ID_INDEXER;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder.Type;
+import com.revrobotics.SparkMaxPIDController;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IndexerSubsystem extends SubsystemBase {
 
   private final CANSparkMax indexer = new CANSparkMax(DEVICE_ID_INDEXER, MotorType.kBrushless);
+  private final RelativeEncoder indexerEncoder;
+  private final SparkMaxPIDController pidController;
 
   public IndexerSubsystem() {
     indexer.restoreFactoryDefaults();
     indexer.enableVoltageCompensation(12);
     indexer.setIdleMode(IdleMode.kCoast);
-    // A small amount of ramp helps prevent voltage drop when the indexer starts up. The voltage drop was causing the
-    // shooter flywheel to slow down.
     indexer.setOpenLoopRampRate(0.1);
+    indexerEncoder = indexer.getAlternateEncoder(Type.kQuadrature, 8192);
+    pidController = indexer.getPIDController();
+    pidController.setFeedbackDevice(indexerEncoder);
     indexer.burnFlash();
   }
 
